3fa901da68ba228fc19592783a3abaa874d61a28
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-04 21:46:49 +0100

    Rename config Parameters

--- src/road_analyzer.cpp
+++ src/road_analyzer.cpp
@@ -2,7 +2,8 @@
 
 bool RoadAnalyzer::initialize() {
     line = readChannel<lms::math::polyLine2f>("LINE");
-    obstacles = readChannel<street_environment::EnvironmentObjects>("OBSTACLES");
+    obstacles =
+        readChannel<street_environment::EnvironmentObjects>("OBSTACLES");
     roadMatrix = writeChannel<street_environment::RoadMatrix>("ROADMATRIX");
     return true;
 }
@@ -12,23 +13,17 @@ bool RoadAnalyzer::deinitialize() {
 }
 
 bool RoadAnalyzer::cycle() {
-    roadMatrix->aroundLine(*line,config().get<int>("widthSteps",2),config().get<float>("stepWidth",0.2),config().get<float>("stepLength",0.4));
-    float deltaBadness =config().get<float>("deltaBadness",1);
-    for(street_environment::EnvironmentObjectPtr ptr:obstacles->objects){
-        if(ptr->getType() == street_environment::Obstacle::TYPE){
-            std::shared_ptr<street_environment::Obstacle> obst = std::dynamic_pointer_cast<street_environment::Obstacle>(ptr);
-            for(const lms::math::vertex2f &v: obst->points()){
-                int x,y;
-                if(roadMatrix->checkBadPosition(v,x,y,deltaBadness)){
-                    /*
-                    logger.error("Inside: ")<<v.x<<" "<<v.y;
-                    logger.error("box:")<<roadMatrix->cell(x,y).points[0].x<<" "<<roadMatrix->cell(x,y).points[0].y;
-                    logger.error("box:")<<roadMatrix->cell(x,y).points[1].x<<" "<<roadMatrix->cell(x,y).points[1].y;
-                    logger.error("box:")<<roadMatrix->cell(x,y).points[2].x<<" "<<roadMatrix->cell(x,y).points[2].y;
-                    logger.error("box:")<<roadMatrix->cell(x,y).points[3].x<<" "<<roadMatrix->cell(x,y).points[3].y;
-                    */
-                }
-
+    roadMatrix->aroundLine(*line, config().get<int>("cellsPerLane", 2),
+                           config().get<float>("cellWidth", 0.2),
+                           config().get<float>("cellLength", 0.4));
+    float deltaBadness = config().get<float>("deltaBadness", 1);
+    for (street_environment::EnvironmentObjectPtr ptr : obstacles->objects) {
+        if (ptr->getType() == street_environment::Obstacle::TYPE) {
+            std::shared_ptr<street_environment::Obstacle> obst =
+                std::dynamic_pointer_cast<street_environment::Obstacle>(ptr);
+            for (const lms::math::vertex2f &v : obst->points()) {
+                int x, y;
+                if (roadMatrix->checkBadPosition(v, x, y, deltaBadness)) {}
             }
         }
     }
@@ -36,4 +31,4 @@ bool RoadAnalyzer::cycle() {
     return true;
 }
 
-void RoadAnalyzer::configsChanged(){}
+void RoadAnalyzer::configsChanged() {}
