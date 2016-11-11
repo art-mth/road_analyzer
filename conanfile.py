67bb95c84ebdec8fdf7081d8dadcd59c00748f5b
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-11 16:30:15 +0100

    Fix dependencies

--- conanfile.py
+++ conanfile.py
@@ -3,5 +3,5 @@ from conans import ConanFile, CMake
 class RoadAnalyzerConan(ConanFile):
     name = "road_analyzer"
     version = "0.1"
-    requires = "lms/2.0@lms/stable"
+    requires = "lms/2.0@lms/stable","lms_math/1.0@lms/stable"
     generators = "cmake"
