913f4e50f8c0d78d1ff3a561dca89af9bfe82aab
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-26 12:57:41 +0100

    Add new data channels that read BoundingBoxes

--- include/road_analyzer.h
+++ include/road_analyzer.h
@@ -4,6 +4,7 @@
 #include <lms/math/polyline.h>
 #include <lms/math/vertex.h>
 #include <lms/module.h>
+#include <street_environment/bounding_box.h>
 #include <street_environment/roadmatrix.h>
 #include <street_environment/street_environment.h>
 
@@ -21,7 +22,8 @@ class RoadAnalyzer : public lms::Module {
 
    private:
     lms::ReadDataChannel<lms::math::polyLine2f> centerLine;
-    lms::ReadDataChannel<street_environment::EnvironmentObjects> obstacles;
+    lms::ReadDataChannel<bool> newObstacles;
+    lms::ReadDataChannel<street_environment::BoundingBoxVector> obstacles;
     lms::WriteDataChannel<street_environment::RoadMatrix> roadMatrix;
 };
 
--- src/road_analyzer.cpp
+++ src/road_analyzer.cpp
@@ -2,8 +2,8 @@
 
 bool RoadAnalyzer::initialize() {
     centerLine = readChannel<lms::math::polyLine2f>("CENTER_LINE");
-    obstacles =
-        readChannel<street_environment::EnvironmentObjects>("OBSTACLES");
+    newObstacles = readChannel<bool>("NEW_OBSTACLES");
+    obstacles = readChannel<street_environment::BoundingBoxVector>("OBSTACLES");
     roadMatrix = writeChannel<street_environment::RoadMatrix>("ROADMATRIX");
     return true;
 }
@@ -14,7 +14,6 @@ bool RoadAnalyzer::cycle() {
     roadMatrix->aroundLine(*centerLine, config().get<float>("laneWidth", 0.4),
                            config().get<int>("cellsPerLane", 4),
                            config().get<float>("cellLength", 0.1));
-    roadMatrix->markEnvironmentObjects(obstacles->objects);
 
     return true;
 }
