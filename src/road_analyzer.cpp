02aa2e27c31fe541cdcbd38a6c794bcad02eb8c5
phibedy <philipp.schmette@gmx.de>
2016-12-01 14:13:04 +0100

    added TODO

--- src/road_analyzer.cpp
+++ src/road_analyzer.cpp
@@ -14,6 +14,14 @@ bool RoadAnalyzer::initialize() {
 bool RoadAnalyzer::deinitialize() { return true; }
 
 bool RoadAnalyzer::cycle() {
+    //TODO centerLine nach hinter verlängern
+    /*
+    lms::math::polyLine2f line;// = *centerLine;
+
+    //TODO Richtungsänderungs zwischen dem 1. und zweiten Segment nutzen und nicht nur nach hinter verlängern
+    line.points().push_back((centerLine->points()[0]-centerLine->points()[1]));
+    */
+    //TODO Trajektorie nach dem Auto
     roadMatrix->aroundLine(*centerLine, config().get<float>("laneWidth", 0.4),
                            config().get<int>("cellsPerLane", 4),
                            config().get<float>("cellLength", 0.1));
